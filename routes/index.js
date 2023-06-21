2660afbb3947afd6b7484696ca4a294d6ca04497
Raftouch <rafanelkin@gmail.com>
2023-06-21 13:00:55 +0200

    :bento: created recently joined page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Mascot = require('../models/mascot')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  let mascots
+  try {
+    mascots = await Mascot.find().sort({ joinedAt: 'desc' }).limit(10).exec()
+  } catch (error) {
+    mascots = []
+  }
+  res.render('index', { mascots: mascots })
 })
 
 module.exports = router
