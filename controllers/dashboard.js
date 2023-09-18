f30ce01b16230817c4d2bf1abfefe603bbed9db6
Raftouch <rafanelkin@gmail.com>
2023-09-18 14:11:32 +0200

    :green_heart: fixed ci

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,14 +3,14 @@ const Mascot = require('../models/mascot')
 const dashboard = async (req, res) => {
   let mascots
   try {
-    mascots = await Mascot.find().sort({ joinedAt: 'desc' }).limit(10).exec()
+    mascots = await Mascot.find().sort({ joinedAt: 'desc' }).limit(12).exec()
   } catch (error) {
     mascots = []
   }
 
   res.render('dashboard/index', {
     mascots: mascots,
-    layout: '../views/layouts/main'
+    layout: '../views/layouts/main',
   })
 }
 
